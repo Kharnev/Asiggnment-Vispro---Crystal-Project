16d61c32416461c71ee021093638ae00232e68a5
Kharnev <143699287+Kharnev@users.noreply.github.com>
2024-10-02 04:22:55 +0800

    Add files via upload

---
+++ Asignment Database/Asignment Database/FormPengguna.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asignment_Database
+{
+    public partial class FormPengguna : Form
+    {
+        public FormPengguna()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReport11_InitReport(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
